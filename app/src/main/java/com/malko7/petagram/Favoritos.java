8fe695744df85c1913d2cf4755606e61a56b4164
Malco Jesse Cruz Santos <isc.malko7@gmail.com>
2016-05-12 19:02:36 -0500

    Toolbar
    Se agrega el toolbar a los layouts

--- app/src/main/java/com/malko7/petagram/Favoritos.java
+++ app/src/main/java/com/malko7/petagram/Favoritos.java
@@ -1,27 +1,27 @@
 package com.malko7.petagram;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class Favoritos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favoritos);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_favoritos, menu);
         return true;
     }
 
@@ -31,12 +31,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        if (id == R.id.action_favoritos) {
-            Intent intent = new Intent(MainActivity.this, Favoritos.class);
-            startActivity(intent);
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
